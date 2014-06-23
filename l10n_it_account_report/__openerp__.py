761c9d91fd6bfd96294325b782b1eb8e7df6e40c
sergiocorato <sergiocorato@gmail.com>
2014-06-23 21:47:16 +0200

    add test for missing company bank in invoice template l10n_it_account_report

--- l10n_it_account_report/__openerp__.py
+++ l10n_it_account_report/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Italian Localisation - Accounting reports',
-    'version': '2.1.9.1',
+    'version': '2.1.9.2',
     'category': 'Localisation/Italy',
     'complexity': "easy",
     'description': """Accounting reports for Italian localization - Fattura
