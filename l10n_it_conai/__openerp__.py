e014e251b49ab4f363d0d328ed342cbd46ed8f21
sergiocorato <sergiocorato@gmail.com>
2014-06-30 00:11:39 +0200

    initial commit

---
+++ l10n_it_conai/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2010-2013 Associazione OpenERP Italia
+#    (<http://www.openerp-italia.org>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Italian CONAI management',
+    'version': '3.0.0.0',
+    'category': 'Accounting',
+    'description': """This module customizes OpenERP for CONAI
+
+Functionalities:
+CONAI
+
+""",
+    'author': 'Didotech srl',
+    'website': 'http://www.didotech.com',
+    'license': 'AGPL-3',
+    "depends": [
+        'account',
+        'stock',
+    ],
+    "data": [
+        'stock/product_view.xml',
+        'partner/partner_view.xml',
+        'partner/declaration_view.xml',
+    ],
+    "demo": [],
+    "active": False,
+    "installable": True
+}
---
+++ l10n_it_conai/partner/partner.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Didotech srl
+#    (<http://www.didotech.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+
+
+class conai_declarations(orm.Model):
+    _name = "conai.declaration"
+    
+    _columns = {
+        'name': fields.char('Name', size=64),
+        'number': fields.char('Number', size=64,),
+        'percent_exemption': fields.float('Exemption %', digits=(6, int(3))),
+        'declaration_id': fields.integer('Declaration id', ),
+        'partner_id': fields.many2one(
+            'res.partner', 'Partner which declaration is linked'),
+        'date_start_validity': fields.date('Date start validity'),
+        'date_end_validity': fields.date('Date end validity'),
+        'active': fields.boolean('Active',),
+        'product_categ_id': fields.many2one(
+            'product.category', 'Product category for which the declaration is valid'),
+        'plafond_amount': fields.float('Amount of plafond', digits=(6, int(3))),
+        'uom_id': fields.many2one('product.uom', 'UOM of plafond amount'),
+    }
+
+    _defaults = {
+        'active': True,
+    }
+
+
+class res_partner(orm.Model):
+    _inherit = "res.partner"
+    
+    _columns = {
+        'name': fields.char('Name', size=64),
+        'conai_declaration_ids': fields.one2many(
+            'conai.declaration', 'declaration_id', 'CONAI declarations'),
+    }
