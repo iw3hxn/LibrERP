cb1a5022a286c7d17484b4edc8112fb75da035d1
lanart <andrei.levin@didotech.com>
2014-07-11 17:47:20 +0200

    [task_time_control]: Fix: time calculation, hide "start" button if task is already started

--- task_time_control/wizard/task_time_control_confirm_wizard.py
+++ task_time_control/wizard/task_time_control_confirm_wizard.py
@@ -6,6 +6,7 @@
 #    Pexego Sistemas Informáticos (http://www.pexego.es) All Rights Reserved
 #    $Jesús Ventosinos Mayor$
 #    $Javier Colmenero Fernández$
+#    Copyright (c) 2014 Didotech srl (info at didotech.com)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -29,26 +30,15 @@ from datetime import datetime
 
 class task_time_control_confirm_wizard(osv.osv_memory):
     _name = 'task.time.control.confirm.wizard'
-    
-    def getUserTask(self, cr, uid, context=None):
-        usr_task_id = self.pool.get('time.control.user.task').search(cr, uid, [('user', '=', uid), ('started_task', '=', context.get('default_task_id', False))])
-        if usr_task_id:
-            return self.pool.get('time.control.user.task').browse(cr, uid, usr_task_id[0])
-        else:
-            return False
-      
-    def see_started_tasks(self, cr, uid, context=None):
-        user = self.getUserTask(cr, uid, context)
-        return (user and user.started_task.id or False)
-       
+
     def get_time(self, cr, uid, context=None):
-        user_task = self.getUserTask(cr, uid, context)
-        if user_task:
+        if context.get('user_task_id', False):
+            user_task = self.pool.get('time.control.user.task').browse(cr, uid, context['user_task_id'], context)
             if user_task.work_start:
                 start_datetime = datetime.strptime(user_task.work_start, '%Y-%m-%d %H:%M:%S.%f')
             else:
                 start_datetime = datetime.now()
-            
+
             if user_task.work_end:
                 end_datetime = datetime.strptime(user_task.work_end, '%Y-%m-%d %H:%M:%S.%f')
             else:
@@ -56,7 +46,10 @@ class task_time_control_confirm_wizard(osv.osv_memory):
             end_seconds = time.mktime(end_datetime.timetuple())
             start_seconds = time.mktime(start_datetime.timetuple())
             diff_hours = (end_seconds - start_seconds) / 60 / 60
-        return (user_task and diff_hours or 0.00)
+
+            return (user_task and diff_hours or 0.00)
+        else:
+            return 0.00
 
     _columns = {
         'task_to_start': fields.many2one('project.task', 'Task to init'),
@@ -65,17 +58,16 @@ class task_time_control_confirm_wizard(osv.osv_memory):
         'name': fields.char('name', size=128),
         'time': fields.float('time')
     }
-       
+
     _defaults = {
         'time': get_time,
-        'started_task': see_started_tasks
     }
-    
+
     def close_confirm(self, cr, uid, ids, context=None):
-        user_task_obj = self.pool.get('time.control.user.task')
-        project_task_obj = self.pool.get('project.task')
-        
-        wizard = self.browse(cr, uid, ids[0])
+        user_task_obj = self.pool['time.control.user.task']
+        project_task_obj = self.pool['project.task']
+
+        wizard = self.browse(cr, uid, ids[0], context)
         user_task = wizard.user_task
         started_task = user_task.started_task
 
@@ -88,7 +80,8 @@ class task_time_control_confirm_wizard(osv.osv_memory):
                 end_datetime = datetime.strptime(user_task.work_end, '%Y-%m-%d %H:%M:%S.%f')
             else:
                 end_datetime = datetime.now()
-            args = {
+
+            self.pool["project.task.work"].create(cr, uid, {
                 'name': wizard.name,
                 'date': end_datetime.strftime('%d-%m-%Y %H:%M:%S'),
                 'task_id': started_task.id,
@@ -97,15 +90,14 @@ class task_time_control_confirm_wizard(osv.osv_memory):
                 'company_id': started_task.company_id and started_task.company_id.id or False,
                 'work_start': start_datetime,
                 'work_end': end_datetime
-            }
-            self.pool.get("project.task.work").create(cr, uid, args)
-            
+            })
+
             user_task_obj.write(cr, uid, user_task.id, {'work_start': None, 'work_end': None, 'started_task': None})
-            
+
             count_other_users_in_task = user_task_obj.search(cr, uid, [('started_task', '=', started_task.id)], count=True)
             if count_other_users_in_task == 0:
                 project_task_obj.write(cr, uid, started_task.id, {'state': 'open'})
-            
+
             if wizard.task_to_start.id:
                 start_id = wizard.task_to_start.id
                 if wizard.task_to_start.state == 'draft':
