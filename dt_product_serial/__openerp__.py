d3cb218d702dc394e0fa9669a7fb65f07c34b044
Carlo <carlo.vettore@didotech.com>
2014-11-01 12:54:47 +0100

    [dt_product_serial] better usability on editable = top

--- dt_product_serial/__openerp__.py
+++ dt_product_serial/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Unique serial number management",
-    "version": "3.3.8.5",
+    "version": "3.3.8.6",
     "author": "Akretion, NaN·tic, Didotech SRL",
     "website": "http://www.akretion.com",
     "depends": [
