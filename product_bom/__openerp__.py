fdfbc51b6a88d43ed7927ad06b86159e7751f475
Carlo <carlo.vettore@didotech.com>
2014-11-05 18:14:19 +0100

    [product_bom] fix KIT button visible also not only on extened view

--- product_bom/__openerp__.py
+++ product_bom/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Show Bom in Product",
-    "version": "3.0.6.3",
+    "version": "3.0.6.4",
     "category": "Sales Management",
     "description": """
         This module adds the 'BOM' on Product. And also use BOM as WBS
