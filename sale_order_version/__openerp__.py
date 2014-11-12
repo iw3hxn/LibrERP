d43cafc8285b09c16f190d670faf17b6e61e3ffa
Carlo <carlo.vettore@didotech.com>
2014-11-12 18:49:18 +0100

    [sale_order_version] erase unused fnt

--- sale_order_version/__openerp__.py
+++ sale_order_version/__openerp__.py
@@ -31,7 +31,7 @@
         
         El filtro por pedido de venta activo, no funciona en la web.
         """,
-    "version": "3.3.10.2",
+    "version": "3.3.10.3",
     "author": "Pexego, Didotech SRL",
     "website": "http://www.pexego.es",
     "category": "Sales/Version",
