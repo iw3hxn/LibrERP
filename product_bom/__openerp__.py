41afbbd0ac8b80f429dda87fb58ba9274f2aae01
Carlo <carlo.vettore@didotech.com>
2014-11-12 10:56:42 +0100

    [product_bom] on view delete readonly because on save not pass parameter

--- product_bom/__openerp__.py
+++ product_bom/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Show Bom in Product",
-    "version": "3.0.6.4",
+    "version": "3.0.6.5",
     "category": "Sales Management",
     "description": """
         This module adds the 'BOM' on Product. And also use BOM as WBS
