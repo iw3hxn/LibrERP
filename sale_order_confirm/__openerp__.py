eca9918367a55273d7334a62d6f68f7e7d5e367e
Carlo <carlo.vettore@didotech.com>
2014-12-01 15:57:00 +0100

    [sale_order_confirm] fix error on kanban view Print action

--- sale_order_confirm/__openerp__.py
+++ sale_order_confirm/__openerp__.py
@@ -29,7 +29,7 @@
 
 {
     "name": "Extend sale_order",
-    "version": "3.8.18.26",
+    "version": "3.8.18.27",
     "category": "Sales Management",
     "description": """This Module, provided user's wizard to confirm/modify sale order.
     It also increases usability on sale order using credit limit amd default payment term
