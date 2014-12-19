0b1af940e43c239d1636b7b4501a1702835bddf7
sergio <sergio.corato@didotech.com>
2014-12-19 16:00:43 +0100

    show abi cab related to bank_riba_id in riba view instead of iban o bank_ids

--- l10n_it_ricevute_bancarie/account/account.py
+++ l10n_it_ricevute_bancarie/account/account.py
@@ -64,6 +64,8 @@ class account_move_line(osv.osv):
             type='boolean', string='RiBa', store=False),
         'unsolved_invoice_ids': fields.many2many('account.invoice', 'invoice_unsolved_line_rel', 'line_id', 'invoice_id', 'Unsolved Invoices'),
         'iban' : fields.related('partner_id', 'bank_ids', 'iban', type='char', string='IBAN', store=False),
+        'abi' : fields.related('partner_id', 'bank_riba_id', 'abi', type='char', string='ABI', store=False),
+        'cab' : fields.related('partner_id', 'bank_riba_id', 'cab', type='char', string='CAB', store=False),
     }
     _defaults = {
         'distinta_line_ids' : None,
