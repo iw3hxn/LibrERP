d5d152277adc4aced81e303d3e23b03c9754a3e7
sergio <sergio.corato@didotech.com>
2015-01-05 09:53:08 +0100

    add notebook in invoice line for later use in intrastat

--- l10n_it_account/__openerp__.py
+++ l10n_it_account/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Italian Localisation - Account Didotech',
-    'version': '2.10.18.17',
+    'version': '2.10.18.18',
     'category': 'Localisation/Italy',
     'description': """This module customizes OpenERP in order to fit italian laws and mores - Account version
 
