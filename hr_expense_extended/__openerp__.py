a5b4b13ede6411eef983e5c55fd7e41d14dbe5d5
Carlo <carlo.vettore@didotech.com>
2015-01-15 12:22:32 +0100

    [hr_expense_extended] now on report is visible also descriptio of spent

--- hr_expense_extended/__openerp__.py
+++ hr_expense_extended/__openerp__.py
@@ -24,7 +24,7 @@
 
 {
     "name": "HR Expense Extended",
-    "version": "2.6.11.0",
+    "version": "2.6.11.1",
     "author": "Didotech SRL",
     "website": "http://www.didotech.com",
     "category": 'Human Resources',
