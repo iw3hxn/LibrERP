3603eef174ef66bfec1c32970f2608a0c470db6c
Carlo <carlo.vettore@didotech.com>
2015-01-28 16:46:18 +0100

    [material_asset] fix security

--- material_asset/__openerp__.py
+++ material_asset/__openerp__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Asset Logistic Management',
-    'version': '3.4.18.4',
+    'version': '3.4.18.5',
     'category': 'Generic Modules/Asset Logistic Management',
     'description': """
         A generic module to manage company asset and its movements.
