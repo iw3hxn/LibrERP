8e6472752acb1c90f08ee7b3a07137647d7b92f0
sergio <sergio.corato@didotech.com>
2015-02-10 23:56:20 +0100

    update l10n_it_account for split payment tax and report

--- l10n_it_account/__openerp__.py
+++ l10n_it_account/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Italian Localisation - Account Didotech',
-    'version': '2.11.20.21',
+    'version': '2.11.21.22',
     'category': 'Localisation/Italy',
     'description': """This module customizes OpenERP in order to fit italian laws and mores - Account version
 
