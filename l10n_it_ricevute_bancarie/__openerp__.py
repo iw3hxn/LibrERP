1806890a87f969b1d0aa4b2b698ac46cb38bbdf9
sergio <sergio.corato@didotech.com>
2015-02-18 23:47:56 +0100

    [FIX] default date and security check l10n_it_ricevute_bancarie

--- l10n_it_ricevute_bancarie/__openerp__.py
+++ l10n_it_ricevute_bancarie/__openerp__.py
@@ -27,7 +27,7 @@
 
 {
     "name": "Ricevute Bancarie",
-    "version": "2.4.11.9",
+    "version": "2.5.12.9",
     "author": "OpenERP Italian Community",
     "category": "Accounting & Finance",
     "website": "http://www.openerp-italia.org",
