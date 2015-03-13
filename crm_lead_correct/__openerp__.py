6893cc7debc7d2af7f6d6d3e74e5c6d60eeffe89
Carlo <carlo.vettore@didotech.com>
2015-03-13 09:39:33 +0100

    [FIX] crm_lead_correct

--- crm_lead_correct/__openerp__.py
+++ crm_lead_correct/__openerp__.py
@@ -29,7 +29,7 @@
 
 {
     'name': 'Module corrects a bug in crm_lead, when creating a partner. Parameter customer is True.',
-    'version': '4.1.11.5',
+    'version': '4.1.12.6',
     'category': 'Customer Relationship Management',
     'description': """A module for crm. Extended by Didotech """,
     "author": "Didotech SRL",
