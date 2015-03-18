5c2f210ec006a82270e6c7aa496e87f335ac70d4
sergio <sergio.corato@didotech.com>
2015-03-18 01:11:21 +0100

    [I18N] translation

--- account_financial_report_horizontal/__openerp__.py
+++ account_financial_report_horizontal/__openerp__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 {
     "name": "Accounting Financial Reports Horizontal",
-    "version": "2.1.4.4",
+    "version": "2.1.4.5",
     "author": ["Therp BV", "Agile Business Group", "Didotech srl"],
     "category": 'Accounting & Finance',
     'complexity': "normal",
