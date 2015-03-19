4da26daa88b4f5b1f671b102b34d3a8159e8bff1
Carlo <carlo.vettore@didotech.com>
2015-03-19 15:41:59 +0100

    [TRA]

--- l10n_it_account/__openerp__.py
+++ l10n_it_account/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Italian Localisation - Account Didotech',
-    'version': '2.13.23.24',
+    'version': '2.13.23.25',
     'category': 'Localisation/Italy',
     'description': """This module customizes OpenERP in order to fit italian laws and mores - Account version
 
