fe718c0456734a4b9f1c9a239e0452a7f4891091
sergio <sergio.corato@didotech.com>
2015-03-29 19:33:21 +0200

    [FIX] value for tax amount

--- account_invoice_entry_date/__openerp__.py
+++ account_invoice_entry_date/__openerp__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Account Invoice entry Date',
-    'version': '3.2.5.4',
+    'version': '3.2.6.4',
     'category': 'Generic Modules/Accounting',
     'description': """This module allows to specify the date to be used by the move created while confirming (supplier) invoice""",
     'author': 'OpenERP Italian Community',
