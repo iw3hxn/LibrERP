6ecf39a80185b08c6afa7bc0131cd4f2360f5d91
Carlo <carlo.vettore@didotech.com>
2015-06-04 22:53:15 +0200

    [fix] inherit of view

--- stock_picking_extended/__openerp__.py
+++ stock_picking_extended/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Stock picking extended',
-    'version': '3.15.14.16',
+    'version': '3.15.14.17',
     'category': 'Others',
     'description': """LibrERP - Stock picking extension
 
