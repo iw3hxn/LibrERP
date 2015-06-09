58a62e96ff66083420813c16f03b06ed47ac818e
Carlo <carlo.vettore@didotech.com>
2015-06-09 17:00:06 +0200

    [work_order] usability view

--- work_order/__openerp__.py
+++ work_order/__openerp__.py
@@ -24,7 +24,7 @@
 
 {
     "name": "Job Order management",
-    "version": "3.16.33.23",
+    "version": "3.16.33.24",
     "author": "Didotech SRL",
     "website": "http://www.didotech.com",
     "category": "Sales Management",
