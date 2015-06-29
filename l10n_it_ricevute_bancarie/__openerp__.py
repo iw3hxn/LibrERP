9e0301a84d488ce1c575dc8c98db675bdd1997ca
Carlo <carlo.vettore@didotech.com>
2015-06-29 11:34:21 +0200

    [FIX] now is not possible to create Riba manualy

--- l10n_it_ricevute_bancarie/__openerp__.py
+++ l10n_it_ricevute_bancarie/__openerp__.py
@@ -27,7 +27,7 @@
 
 {
     "name": "Ricevute Bancarie",
-    "version": "2.7.18.15",
+    "version": "2.7.18.16",
     "author": "OpenERP Italian Community",
     "category": "Accounting & Finance",
     "website": "http://www.openerp-italia.org",
