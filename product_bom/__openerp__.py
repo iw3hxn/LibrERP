12af940210c7e81b6248ce5f8f51baff67f28a03
Carlo <carlo.vettore@didotech.com>
2015-07-01 09:11:23 +0200

    [product_bom] increase usability

--- product_bom/__openerp__.py
+++ product_bom/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Show Bom in Product",
-    "version": "3.1.6.6",
+    "version": "3.1.6.7",
     "category": "Sales Management",
     "description": """
         This module adds the 'BOM' on Product. And also use BOM as WBS
