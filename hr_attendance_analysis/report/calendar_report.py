a09b38be08965206e1b9d27c10e9467af9a499e6
Carlo <carlo.vettore@didotech.com>
2015-07-10 11:56:29 +0200

    [hr_attendance_analysis] fix timezone report

--- hr_attendance_analysis/report/calendar_report.py
+++ hr_attendance_analysis/report/calendar_report.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    Copyright (C) 2011 Agile Business Group sagl (<http://www.agilebg.com>)
 #    Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
 #
@@ -20,35 +20,30 @@
 ##############################################################################
 
 import time
-from report import report_sxw
-from osv import osv
 from datetime import datetime
-from tools.translate import _
+from openerp.report import report_sxw
+from openerp.tools.translate import _
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
+
 
 class Parser(report_sxw.rml_parse):
 
     def _get_day_of_week(self, day):
-        dayofweek=''
-        weekday = datetime.strptime(day,'%Y-%m-%d').weekday()
-        if weekday == 0:
-            dayofweek = _('Monday')
-        elif weekday == 1:
-            dayofweek = _('Tuesday')
-        elif weekday == 2:
-            dayofweek = _('Wednesday')
-        elif weekday == 3:
-            dayofweek = _('Thursday')
-        elif weekday == 4:
-            dayofweek = _('Friday')
-        elif weekday == 5:
-            dayofweek = _('Saturday')
-        elif weekday == 6:
-            dayofweek = _('Sunday')
-        return dayofweek
+        WEEKDAYS = {
+            0: _('Monday'),
+            1: _('Tuesday'),
+            2: _('Wednesday'),
+            3: _('Thursday'),
+            4: _('Friday'),
+            5: _('Saturday'),
+            6: _('Sunday'),
+        }
+        weekday = datetime.strptime(day, DEFAULT_SERVER_DATE_FORMAT).weekday()
+        return WEEKDAYS[weekday]
 
     def _get_month_name(self, day):
-        str_month=''
-        month = datetime.strptime(day,'%Y-%m-%d').month
+        str_month = ''
+        month = datetime.strptime(day, DEFAULT_SERVER_DATE_FORMAT).month
         if month == 1:
             str_month = _('January')
         elif month == 2:
@@ -76,14 +71,17 @@ class Parser(report_sxw.rml_parse):
         return str_month
 
     def _get_days_by_employee(self, employee_id):
-        return self.localcontext['data']['form']['days_by_employee'][str(employee_id)]
+        form = self.localcontext['data']['form']
+        return form['days_by_employee'][str(employee_id)]
 
     def _get_totals_by_employee(self, employee_id):
-        return self.localcontext['data']['form']['totals_by_employee'][str(employee_id)]
+        form = self.localcontext['data']['form']
+        return form['totals_by_employee'][str(employee_id)]
 
     def _get_max_per_day(self):
-        return self.localcontext['data']['form']['max_number_of_attendances_per_day']
-        
+        form = self.localcontext['data']['form']
+        return form['max_number_of_attendances_per_day']
+
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)
         self.localcontext.update({
@@ -96,6 +94,6 @@ class Parser(report_sxw.rml_parse):
         })
 
 report_sxw.report_sxw('report.attendance_analysis.calendar_report',
-                       'attendance_analysis.calendar_report', 
-                       'attendance_analysis/report/calendar_report.mako',
-                       parser=Parser)
+                      'attendance_analysis.calendar_report',
+                      'attendance_analysis/report/calendar_report.mako',
+                      parser=Parser)
