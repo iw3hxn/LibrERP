c816bc97f2feb6d33b02d25134f44f96e27ec609
Carlo <carlo.vettore@didotech.com>
2015-07-22 16:42:39 +0200

    [fix] sale_bom

--- sale_bom/__openerp__.py
+++ sale_bom/__openerp__.py
@@ -19,7 +19,7 @@
 
 {
     "name": "Bom in Sales Orders",
-    "version":"3.1.5.11",
+    "version": "3.1.5.12",
     "category": "Sales Management",
     "description": """
 This module adds the 'BOM' on sales order.
