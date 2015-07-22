9cbb4f531dd404740614d68d69bdcc174d1cb106
Carlo <carlo.vettore@didotech.com>
2015-07-22 14:13:09 +0200

    [revision]

--- data_migration/__openerp__.py
+++ data_migration/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Data migration import',
-    'version': '2.3.32.6',
+    'version': '2.4.33.7',
     'category': 'Tools',
     'description': """
         This module gives a possibilitie to import products and partners
