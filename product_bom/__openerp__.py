49de53b3a6212aaca312325e9530ab4428c875ef
Carlo <carlo.vettore@didotech.com>
2015-08-18 10:54:33 +0200

    [FIX] security

--- product_bom/__openerp__.py
+++ product_bom/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Show Bom in Product",
-    "version": "3.2.6.8",
+    "version": "3.2.6.9",
     "category": "Sales Management",
     "description": """
         This module adds the 'BOM' on Product. And also use BOM as WBS
