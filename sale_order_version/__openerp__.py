e65e2033a807edd016129056b760c27c1f3e5794
Carlo <carlo.vettore@didotech.com>
2015-08-19 21:47:10 +0200

    [FIX] as required from 3P

--- sale_order_version/__openerp__.py
+++ sale_order_version/__openerp__.py
@@ -31,7 +31,7 @@
         
         El filtro por pedido de venta activo, no funciona en la web.
         """,
-    "version": "3.3.10.6",
+    "version": "3.3.10.7",
     "author": "Pexego, Didotech SRL",
     "website": "http://www.pexego.es",
     "category": "Sales/Version",
