c11fb1658cbc72365a7f20695637adb0ba1bf1ed
Carlo <carlo.vettore@didotech.com>
2015-08-24 16:09:58 +0200

    [partner_blacklist] context missing

--- partner_blacklist/partner.py
+++ partner_blacklist/partner.py
@@ -7,42 +7,42 @@ from osv.orm import except_orm
 from tools.translate import _
 
 COLOR_SELECTION = [
-         ('aqua', (u"Aqua")),
-         ('black', (u"Black")),
-         ('blue', (u"Blue")),
-         ('brown', (u"Brown")),
-         ('cadetblue', (u"Cadet Blue")),
-         ('darkblue', (u"Dark Blue")),
-         ('fuchsia', (u"Fuchsia")),
-         ('forestgreen' , (u"Forest Green")),
-         ('green', (u"Green")),
-         ('grey', (u"Grey")),  
-         ('red', (u"Red")),
-         ('orange', (u"Orange"))
+    ('aqua', (u"Aqua")),
+    ('black', (u"Black")),
+    ('blue', (u"Blue")),
+    ('brown', (u"Brown")),
+    ('cadetblue', (u"Cadet Blue")),
+    ('darkblue', (u"Dark Blue")),
+    ('fuchsia', (u"Fuchsia")),
+    ('forestgreen', (u"Forest Green")),
+    ('green', (u"Green")),
+    ('grey', (u"Grey")),
+    ('red', (u"Red")),
+    ('orange', (u"Orange"))
 ]
 
 
 class res_partner(osv.osv):
     _inherit = "res.partner"
-    
+
     def get_color(self, cr, uid, ids, field_name, arg, context):
         value = {}
-        partners = self.browse(cr, uid, ids)
+        partners = self.browse(cr, uid, ids, context)
         for partner in partners:
             if partner.blacklist:
                 value[partner.id] = 'red'
             else:
                 value[partner.id] = 'black'
         return value
-    
+
     def onchange_blacklist(self, cr, uid, ids, blacklist, context=None):
-        res = {}       
+        res = {}
         if blacklist:
             res['sale_warn'] = 'warning'
         return {'value': res}
-    
+
     _columns = {
-        'blacklist': fields.boolean('Cliente bloccato', help="Ricorda di selezionare il messaggio da mostrare sui preventivi"),
-        'row_color': fields.function(get_color, string = 'Row color', type='char', readonly=True, method=True,)
+        'blacklist': fields.boolean('Blocked Partner', help="Remember to set advise on warning section"),
+        'row_color': fields.function(get_color, string='Row color', type='char', readonly=True, method=True)
     }
 
