74d5ee529187d735acaafa99983f195a4caa6027
Carlo <carlo.vettore@didotech.com>
2015-09-10 16:53:38 +0200

    [crm_lead_correct] correct usability on opportunity form view

--- crm_lead_correct/__openerp__.py
+++ crm_lead_correct/__openerp__.py
@@ -29,7 +29,7 @@
 
 {
     'name': 'Module corrects a bug in crm_lead, when creating a partner. Parameter customer is True.',
-    'version': '4.3.14.10',
+    'version': '4.3.14.11',
     'category': 'Customer Relationship Management',
     'description': """A module for crm. Extended by Didotech """,
     "author": "Didotech SRL",
