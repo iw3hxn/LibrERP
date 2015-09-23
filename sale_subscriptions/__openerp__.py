3a3e292c7181a0cec22a0ab980069e6b7e3388f6
Andrei Levin <andrei.levin@didotech.com>
2015-09-23 22:36:08 +0200

    [sale_subscription]: Added possibility to suspend orders

--- sale_subscriptions/__openerp__.py
+++ sale_subscriptions/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     "name": "Subscrption Sale",
-    "version": "3.1.8.5",
+    "version": "3.1.9.6",
     "author": "Agile Business Group & Domsense & Didotech",
     'website': 'http://www.didotech.com',
     'category': 'Generic Modules/Sales & Purchases',
