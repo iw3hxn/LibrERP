a458e8589c5888d5789aa27c7f2ef5ce9ffa0a10
Carlo <carlo.vettore@didotech.com>
2015-12-15 12:09:26 +0100

    new search view

--- sale_subscriptions/__openerp__.py
+++ sale_subscriptions/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     "name": "Subscrption Sale",
-    "version": "3.2.17.10",
+    "version": "3.2.17.11",
     "author": "Agile Business Group & Domsense & Didotech",
     'website': 'http://www.didotech.com',
     'category': 'Generic Modules/Sales & Purchases',
