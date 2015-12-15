bd8cbedd37c0cd144fa29087b84bd618e497c085
Carlo <carlo.vettore@didotech.com>
2015-12-15 12:38:34 +0100

    new store function on customer

--- sale_subscriptions/__openerp__.py
+++ sale_subscriptions/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     "name": "Subscrption Sale",
-    "version": "3.4.17.12",
+    "version": "3.4.17.13",
     "author": "Agile Business Group & Domsense & Didotech",
     'website': 'http://www.didotech.com',
     'category': 'Generic Modules/Sales & Purchases',
