e38f2db26d07f165ffc5602785ffcd3d94fd1a12
andreinl <andrei.levin@didotech.com>
2016-01-05 19:11:04 +0100

    [project_extended] Draft of the faster _total_account() function

--- project_extended/__openerp__.py
+++ project_extended/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Project Extended',
-    'version': '3.3.8.14',
+    'version': '3.4.9.14',
     'category': 'Generic Modules/Projects & Services',
     'description': """Tasks list on a dedicated tab on the project form
 """,
