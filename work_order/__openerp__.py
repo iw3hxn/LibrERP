d8985352735aa7e65ed6feba6040da6f291e528a
Carlo <carlo.vettore@didotech.com>
2016-01-17 23:23:37 +0100

    missing change revision

--- work_order/__openerp__.py
+++ work_order/__openerp__.py
@@ -24,7 +24,7 @@
 
 {
     "name": "Job Order management",
-    "version": "3.16.33.26",
+    "version": "3.16.34.27",
     "author": "Didotech SRL",
     "website": "http://www.didotech.com",
     "category": "Sales Management",
