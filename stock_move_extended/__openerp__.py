2b8b658ea3bd9de7dcd597864638e6be616f2993
Carlo <carlo.vettore@didotech.com>
2016-07-27 14:16:25 +0200

    update revision

--- stock_move_extended/__openerp__.py
+++ stock_move_extended/__openerp__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     "name": "Stock Move Extended",
-    "version": "3.2.3.7",
+    "version": "3.2.3.8",
     "depends": ["stock"],
     "author": "Didotech SRL",
     "category": "Custom Modules",
