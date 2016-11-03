d6aede72c656fe5a2f8214f197fb9569092b4cce
carlo <carlo.vettore@didotech.com>
2016-11-03 17:50:34 +0100

    fix error on emissione riba nota accredito

--- l10n_it_ricevute_bancarie/__openerp__.py
+++ l10n_it_ricevute_bancarie/__openerp__.py
@@ -27,7 +27,7 @@
 
 {
     "name": "Ricevute Bancarie",
-    "version": "2.8.22.19",
+    "version": "2.8.23.20",
     "author": "OpenERP Italian Community",
     "category": "Accounting & Finance",
     "website": "http://www.openerp-italia.org",
--- l10n_it_ricevute_bancarie/wizard/wizard_emissione_riba.py
+++ l10n_it_ricevute_bancarie/wizard/wizard_emissione_riba.py
@@ -35,16 +35,15 @@ class emissione_riba(orm.TransientModel):
     _name = "riba.emissione"
     _description = "Emissione Ricevute Bancarie"
     _columns = {
-
-        'configurazione' : fields.many2one('riba.configurazione', 'Configurazione', required=True),
-        }
+        'configurazione': fields.many2one('riba.configurazione', 'Configurazione', required=True),
+    }
 
     def crea_distinta(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
 
         def create_rdl(conta, rd_id, date_maturity, partner_id, acceptance_account_id, bank_id=None, bank_riba_id=None):
-            rdl = {
+            riba_line_vals = {
                 'sequence': conta,
                 'bank_id': bank_id,
                 'distinta_id': rd_id,
@@ -54,26 +53,26 @@ class emissione_riba(orm.TransientModel):
                 'acceptance_account_id': acceptance_account_id,
                 'bank_riba_id': bank_riba_id,
             }
-            return riba_distinta_line.create(cr, uid, rdl, context=context)
+            return riba_distinta_line_obj.create(cr, uid, riba_line_vals, context=context)
 
         """
         Qui creiamo la distinta
         """
-        wizard_obj = self.browse(cr, uid, ids)[0]
+        wizard_obj = self.browse(cr, uid, ids, context)[0]
         active_ids = context and context.get('active_ids', [])
-        riba_distinta = self.pool.get('riba.distinta')
-        riba_distinta_line = self.pool.get('riba.distinta.line')
-        riba_distinta_move_line = self.pool.get('riba.distinta.move.line')
-        move_line_obj = self.pool.get('account.move.line')
+        riba_distinta_obj = self.pool['riba.distinta']
+        riba_distinta_line_obj = self.pool['riba.distinta.line']
+        riba_distinta_move_line_obj = self.pool['riba.distinta.move.line']
+        move_line_obj = self.pool['account.move.line']
 
         # create distinta
-        rd = {
-            'name': self.pool.get('ir.sequence').get(cr, uid, 'seq.riba.distinta'),
+        riba_vals = {
+            'name': self.pool['ir.sequence'].get(cr, uid, 'seq.riba.distinta'),
             'config': wizard_obj.configurazione.id,
             'user_id': uid,
             'date_created': fields.date.context_today(cr, uid, context),
         }
-        rd_id = riba_distinta.create(cr, uid, rd)
+        rd_id = riba_distinta_obj.create(cr, uid, riba_vals, context)
 
         # group by partner and due date
         grouped_lines = {}
@@ -115,9 +114,9 @@ class emissione_riba(orm.TransientModel):
 #                        total = 0.0
 #                        invoice_date_group = ''
                         for grouped_line in grouped_lines[key]:
-                            riba_distinta_move_line.create(cr, uid, {
+                            riba_distinta_move_line_obj.create(cr, uid, {
                                 'riba_line_id': rdl_id,
-                                'amount': grouped_line.credit and grouped_line.amount_residual * -1 or grouped_line.debit and grouped_line.amount_residual,
+                                'amount': grouped_line.residual,
                                 'move_line_id': grouped_line.id,
                                 }, context=context)
                         del grouped_lines[key]
@@ -129,16 +128,16 @@ class emissione_riba(orm.TransientModel):
                 else:
                     rdl_id = create_rdl(conta, rd_id, move_line.date_maturity, move_line.partner_id.id,
                                     wizard_obj.configurazione.acceptance_account_id.id, bank_id.id, None)
-                riba_distinta_move_line.create(cr, uid, {
+                riba_distinta_move_line_obj.create(cr, uid, {
                     'riba_line_id': rdl_id,
-                    'amount': move_line.amount_residual,
+                    'amount': move_line.residual,
                     'move_line_id': move_line.id,
                     }, context=context)
 
             conta += 1
 
         # ----- show distinta form
-        mod_obj = self.pool.get('ir.model.data')
+        mod_obj = self.pool['ir.model.data']
         res = mod_obj.get_object_reference(cr, uid, 'l10n_it_ricevute_bancarie', 'view_distinta_riba_form')
         res_id = res and res[1] or False,
         return {
