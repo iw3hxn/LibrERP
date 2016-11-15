8b37adf34d22b3abd9b3d33df311c0bdd805febe
carlo <carlo.vettore@didotech.com>
2016-11-15 17:46:37 +0100

    commit also migration script

---
+++ hr_attendance_analysis/migrations/6.1.0.2/pre-migration.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi.
+#    Copyright Camptocamp SA 2011
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+def migrate(cr, version):
+    # only run at first install
+    cr.execute("ALTER TABLE hr_attendance DROP duration, DROP end_datetime, DROP outside_calendar_duration, DROP inside_calendar_duration;")
+
+    import pdb;pdb.set_trace()
+
