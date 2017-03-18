e3ec0e2c86512165aa1597a9fa02c1aa4e1cd9a4
carlo <carlo.vettore@didotech.com>
2017-03-18 09:33:40 +0100

    Initial Commit for Con2Act, add sequence line on purchase order

---
+++ purchase_order_extended/model/inherit_purchase_order_line.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#
+#    Copyright (C) 2017 Didotech srl (<http://www.didotech.com>).
+#
+#                       All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+from datetime import datetime
+
+
+class purchase_order_line(orm.Model):
+
+    _inherit = 'purchase.order.line'
+
+    def _get_order_line_sequence(self, cr, uid, ids, field_name, arg, context):
+        purchase_order_line = self.browse(cr, uid, ids, context)
+
+        result = {}
+        for line in purchase_order_line:
+            result[line.id] = line.order_id.order_line.index(line) + 1
+        return result
+
+    _columns = {
+        'sequence': fields.function(_get_order_line_sequence, string='Line #', type='integer', method=True),
+    }
+
