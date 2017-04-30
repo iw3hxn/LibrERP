eaa48176cbc60c0e6c745d2e3b2463e5a3bbbbaa
carlo <carlo.vettore@didotech.com>
2017-04-30 13:59:16 +0200

    add filter

--- hr_attendance_extended/__openerp__.py
+++ hr_attendance_extended/__openerp__.py
@@ -24,7 +24,7 @@
 
 {
     "name": "HR Expense Extended",
-    "version": "3.0.0.1",
+    "version": "3.0.0.2",
     "author": "Didotech SRL",
     "website": "http://www.didotech.com",
     "category": 'Human Resources',
