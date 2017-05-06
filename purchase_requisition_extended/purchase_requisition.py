5bea462fb77fec0612f32fe4b4a5eb7b62ef35ba
carlo <carlo.vettore@didotech.com>
2017-05-06 18:26:14 +0200

    missing context

--- purchase_requisition_extended/purchase_requisition.py
+++ purchase_requisition_extended/purchase_requisition.py
@@ -78,7 +78,7 @@ class purchase_requisition(orm.Model):
         return True
         
     def request_to_suppliers(self, cr, uid, ids, context):
-        virtual_partner_obj = self.pool.get('virtual.purchase.requisition.partner')
+        virtual_partner_obj = self.pool['virtual.purchase.requisition.partner']
         for requisition in self.browse(cr, uid, ids, context):
             suppliers = virtual_partner_obj._get_requisition_suppliers(cr, uid, context={'active_id': requisition.id})
             if not suppliers:
@@ -86,7 +86,7 @@ class purchase_requisition(orm.Model):
             for supplier_id in suppliers:
                 if supplier_id in filter(lambda x: x, [rfq.state != 'cancel' and rfq.partner_id.id or None for rfq in requisition.purchase_ids]):
                     continue
-                requisition_partner_id = virtual_partner_obj.create(cr, uid, {'partner_id': supplier_id})
+                requisition_partner_id = virtual_partner_obj.create(cr, uid, {'partner_id': supplier_id}, context)
                 virtual_partner_obj.create_order(cr, uid, [requisition_partner_id], context={'active_ids': [requisition.id]})
 
         self.tender_in_progress(cr, uid, ids, context=None)
@@ -185,4 +185,4 @@ class purchase_requisition_line(orm.Model):
         # 'compound_qty': fields.function(_get_compound_qty, string=_('Quantity'), method=True),
         # 'destination_ids': fields.one2many('purchase.requisition.line.destination', 'requisition_line_id', _('Products Distribution'))
     }
-    
+
