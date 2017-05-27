6bcb1892912f821d46ec339446f6091feff481de
carlo <carlo.vettore@didotech.com>
2017-05-27 19:48:10 +0200

    increase usability

--- project_extended/__openerp__.py
+++ project_extended/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Project Extended',
-    'version': '3.13.17.28',
+    'version': '3.13.17.29',
     'category': 'Generic Modules/Projects & Services',
     'description': """Tasks list on a dedicated tab on the project form
 """,
@@ -34,6 +34,7 @@
         'task_time_control',
         'res_users_helper_functions',
         'core_extended',
+        'res_users_kanban'
     ],
     "data": [
         'security/security.xml',
