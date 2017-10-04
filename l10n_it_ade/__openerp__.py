1571c7cb0ca08a4eec858fafcff1ab04fb396837
andreinl <andrei.levin@didotech.com>
2017-10-04 12:29:43 +0200

    [l10n_it_ade] Updated version number

--- l10n_it_ade/__openerp__.py
+++ l10n_it_ade/__openerp__.py
@@ -5,7 +5,7 @@
 #
 {
     "name": "Base xml Agenzia delle Entrate",
-    "version": "6.1.0.2.1",
+    "version": "6.1.1.3.2",
     "category": "Localization/Italy",
     "summary": "Codice con le definizioni dei file xml Agenzia delle Entrate",
     "author": "SHS-AV s.r.l.,"
