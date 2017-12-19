a9d2ea1c0cb6543bda4e7269acdd52b430995a75
Antonio Mignolli <antonio.mignolli@didotech.com>
2017-12-19 15:18:22 +0100

    Code cleaning

--- sale_order_requirement/models/purchase_order.py
+++ sale_order_requirement/models/purchase_order.py
@@ -8,8 +8,6 @@ class purchase_order_line(orm.Model):
     _inherit = 'purchase.order'
 
     _columns = {
-        # 'order_requirement_ids': fields.many2many('order.requirement', string='Order Requirements', readonly=True),
-        # 'order_requirement_line_ids': fields.many2many('order.requirement.line', string='Order Requirement Lines', readonly=True),
         'sale_order_ids': fields.many2many('sale.order', string='Sale Orders', readonly=True),
     }
 
