e772d04617b9a9f6b8e3fc6934a373f4e8ccdc6d
carlo <carlo.vettore@didotech.com>
2018-01-02 10:11:56 +0100

    better view

--- sale_order_requirement/__openerp__.py
+++ sale_order_requirement/__openerp__.py
@@ -1,6 +1,6 @@
 {
     "name": "Sale Order Requirement",
-    "version": "4.18.17.19",
+    "version": "4.18.17.20",
     "author": "Antonio Mignolli - Didotech SRL",
     "category": 'Sales Management',
     "description": """
