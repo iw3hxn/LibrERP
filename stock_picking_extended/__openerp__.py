0f0ea674e63c7ce5bbe662f512c36c840eddc6fe
carlo <carlo.vettore@didotech.com>
2018-01-24 17:08:37 +0100

    fix error on security

--- stock_picking_extended/__openerp__.py
+++ stock_picking_extended/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Stock picking extended',
-    'version': '3.33.33.37',
+    'version': '3.33.33.38',
     'category': 'Others',
     'description': """LibrERP - Stock picking extension
 
