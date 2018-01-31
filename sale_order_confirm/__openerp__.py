9d8b9104e0cb83c78a017e62c79923ab5deeb5de
carlo <carlo.vettore@didotech.com>
2018-01-31 13:28:09 +0100

    better view and filter

--- sale_order_confirm/__openerp__.py
+++ sale_order_confirm/__openerp__.py
@@ -29,7 +29,7 @@
 
 {
     "name": "Extend sale_order",
-    "version": "3.31.61.69",
+    "version": "3.31.61.70",
     "category": "Sales Management",
     "description": """This Module, provided user's wizard to confirm/modify sale order.
     It also increases usability on sale order using credit limit amd default payment term
