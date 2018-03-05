0ee6b7f423d305361eb2ece5c61669d20f05b856
carlo <carlo.vettore@didotech.com>
2018-03-05 16:05:14 +0100

    fix picking filter

--- stock_picking_filter/__openerp__.py
+++ stock_picking_filter/__openerp__.py
@@ -24,7 +24,7 @@
 
 {
     "name": "Account Invoice Extended",
-    "version": "2.0.3.3",
+    "version": "2.0.3.4",
     "author": "Didotech SRL",
     "website": "http://www.didotech.com",
     "category": 'Accounting & Finance',
