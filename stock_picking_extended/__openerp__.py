3a7b9b682e542fe7bac761305fba43b1aa609d68
carlo <carlo.vettore@didotech.com>
2018-07-02 11:15:09 +0200

    from order board change to assigned and not on done

--- stock_picking_extended/__openerp__.py
+++ stock_picking_extended/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Stock picking extended',
-    'version': '3.41.41.50',
+    'version': '3.41.42.51',
     'category': 'Others',
     'description': """LibrERP - Stock picking extension
 
