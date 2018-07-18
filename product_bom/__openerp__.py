0524d13e13fc895b6839fb6baa2964ef19c79bdc
andreinl <andrei.levin@didotech.com>
2018-07-18 17:24:15 +0200

    [product_bom]: Refactoring: UpdateCachePrice()

--- product_bom/__openerp__.py
+++ product_bom/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Show Bom in Product",
-    "version": "3.4.22.15",
+    "version": "3.4.23.15",
     "category": "Sales Management",
     "description": """
         This module adds the 'BOM' on Product. And also use BOM as WBS
