f96dfc540f715eb3886370f8b87cd5befcf53679
carlo <carlo.vettore@didotech.com>
2018-08-22 09:36:31 +0200

    now on Lead Tree view is possibile to see note

--- crm_lead_correct/__openerp__.py
+++ crm_lead_correct/__openerp__.py
@@ -29,7 +29,7 @@
 
 {
     'name': 'Module corrects a bug in crm_lead, when creating a partner. Parameter customer is True.',
-    'version': '4.9.24.21',
+    'version': '4.9.24.22',
     'category': 'Customer Relationship Management',
     'description': """A module for crm. Extended by Didotech """,
     "author": "Didotech SRL",
