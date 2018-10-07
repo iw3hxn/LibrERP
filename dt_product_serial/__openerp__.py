74ce9d97dc52088930cb6adbd12d574de3cffb8f
carlo <carlo.vettore@didotech.com>
2018-10-07 19:50:48 +0200

    fix ... security with low level access

--- dt_product_serial/__openerp__.py
+++ dt_product_serial/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Unique serial number management",
-    "version": "3.8.24.22",
+    "version": "3.8.24.23",
     "author": "Akretion, NaN·tic, Didotech SRL",
     "website": "http://www.akretion.com",
     "depends": [
--- material_asset/__openerp__.py
+++ material_asset/__openerp__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Asset Logistic Management',
-    'version': '3.4.24.13',
+    'version': '3.4.24.15',
     'category': 'Generic Modules/Asset Logistic Management',
     'description': """
         A generic module to manage company asset and its movements.
