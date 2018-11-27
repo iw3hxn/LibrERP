1595dddb861d67222590342222012e24d1b85546
carlo <carlo.vettore@didotech.com>
2018-11-27 19:05:56 +0100

    increase usability

--- stock_picking_extended/__openerp__.py
+++ stock_picking_extended/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Stock picking extended',
-    'version': '3.46.44.57',
+    'version': '3.46.44.58',
     'category': 'Others',
     'description': """LibrERP - Stock picking extension
 
