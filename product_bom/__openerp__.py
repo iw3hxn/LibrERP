b45d10a4904e9a65634ae868451edccefff7e7f7
andreinl <andrei.levin@didotech.com>
2019-01-08 18:02:32 +0100

    [product_bom] Fix: missing context

--- product_bom/__openerp__.py
+++ product_bom/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Show Bom in Product",
-    "version": "3.4.25.17",
+    "version": "3.4.26.17",
     "category": "Sales Management",
     "description": """
         This module adds the 'BOM' on Product. And also use BOM as WBS
