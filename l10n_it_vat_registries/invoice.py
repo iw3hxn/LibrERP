413c7c5143617d07526f76c5a6f479dbb3ca3e88
carlo <carlo.vettore@didotech.com>
2019-02-02 18:11:09 +0100

    fix multi journal

--- l10n_it_vat_registries/invoice.py
+++ l10n_it_vat_registries/invoice.py
@@ -131,8 +131,9 @@ class Parser(report_sxw.rml_parse):
         tax_code_obj = self.pool['account.tax.code']
         for period_id in self.localcontext['data']['period_ids']:
             move_state = ('posted', )
-            journal_ids = (','.join([str(x) for x in self.localcontext['data']['journal_ids']]))
-            sum_p = tax_code_obj._sum(self.cr, self.uid, tax_code_ids, '', '', self.localcontext, where=' AND line.period_id=%s AND move.state IN %s AND line.journal_id IN (%s) ', where_params=(period_id, move_state, journal_ids))
+            journal_ids = self.localcontext['data']['journal_ids']
+            sum_p = tax_code_obj._sum(self.cr, self.uid, tax_code_ids, '', '', self.localcontext, where=' AND line.period_id=%s AND move.state IN %s AND line.journal_id IN %s ',
+                                      where_params=(period_id, move_state, (tuple(journal_ids,))))
 
             for tax_code_id in tax_code_ids:
                 if not res_dict.get(tax_code_id):
