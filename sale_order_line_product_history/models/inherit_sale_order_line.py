e5d35c0360926e5a6dea9fe3a0796516bd6e30ce
carlo <carlo.vettore@didotech.com>
2019-02-11 19:07:26 +0100

    if pos order not calculate.. speedup

--- sale_order_line_product_history/models/inherit_sale_order_line.py
+++ sale_order_line_product_history/models/inherit_sale_order_line.py
@@ -16,11 +16,10 @@ class SaleOrderLine(orm.Model):
                           uom=False, qty_uos=0, uos=False, name='', partner_id=False,
                           lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False,
                           flag=False, context=None):
-
+        context = context or self.pool['res.users'].context_get(cr, uid)
         res = super(SaleOrderLine, self).product_id_change(cr, uid, ids, pricelist, product_id, qty, uom, qty_uos, uos, name, partner_id, lang, update_tax, date_order, packaging, fiscal_position, flag, context)
-        if product_id and partner_id:
+        if product_id and partner_id and context.get('pos_order_type', False):
             product_sell_ids = self.search(cr, uid, [('product_id', '=', product_id), ('order_partner_id', '!=', partner_id)], context=context)
             partner_sell_ids = self.search(cr, uid, [('product_id', '=', product_id), ('order_partner_id', '=', partner_id)], context=context)
-
             res['value']['product_sale_order_history_ids'] = partner_sell_ids + product_sell_ids
         return res
