5e6b9cdd23544d23017c56d03e02d9b2300a8ee6
carlo <carlo.vettore@didotech.com>
2019-03-06 15:51:14 +0100

    supplier is not required if i use warehouse product

--- sale_order_requirement/__openerp__.py
+++ sale_order_requirement/__openerp__.py
@@ -1,6 +1,6 @@
 {
     "name": "Sale Order Requirement",
-    "version": "4.35.47.44",
+    "version": "4.35.47.45",
     "author": "Antonio Mignolli - Didotech SRL",
     "category": 'Sales Management',
     "description": """
