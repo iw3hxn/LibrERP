3c69d7f2d56c5f20735fbe6ba7db1fd33574e8f5
carlo <carlo.vettore@didotech.com>
2019-05-20 10:52:18 +0200

    fix view

--- account_vat_period_end_statement/__openerp__.py
+++ account_vat_period_end_statement/__openerp__.py
@@ -15,7 +15,7 @@
 # [2017: odoo-italia] Electronic VAT statement
 {
     'name': 'Period End VAT Statement',
-    'version': '7.0.4.0.5',
+    'version': '7.0.4.0.6',
     'category': 'Generic Modules/Accounting',
     'author': 'Agile Business Group,'
               ' Odoo Italia Associazione,'
