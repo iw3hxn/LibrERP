be2da1dd052f3edce3697513f657f8312b3c36a4
carlo <carlo.vettore@didotech.com>
2019-07-05 11:10:42 +0200

    [sale_order_requirement] new report

--- sale_order_requirement/__openerp__.py
+++ sale_order_requirement/__openerp__.py
@@ -1,6 +1,6 @@
 {
     "name": "Sale Order Requirement",
-    "version": "4.37.51.48",
+    "version": "4.37.51.49",
     "author": "Antonio Mignolli - Didotech SRL",
     "category": 'Sales Management',
     "description": """
@@ -29,6 +29,7 @@ and choose if manufacture or buy products from suppliers.
         'views/sale_workflow.xml',
         'views/sale_order_view.xml',
         'report/order_requirement.xml',
+        'report/stock_picking_in_report.xml'
  #       'report/incoming_bill.xml',
     ],
     'css': ['static/src/css/style.css'],
