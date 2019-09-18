e197452fe764ab35d9c2d68b530708bbef23a78f
carlo <carlo.vettore@didotech.com>
2019-09-18 15:38:24 +0200

    [fix] recalculate for all

--- project_extended/models/inherited_sale_order_line.py
+++ project_extended/models/inherited_sale_order_line.py
@@ -31,6 +31,6 @@ class sale_order_line(orm.Model):
 
     _columns = {
         'price_subtotal': fields.function(_amount_line, string='Subtotal', digits_compute=dp.get_precision('Sale Price'), store={
-                'sale.order.line': (lambda self, cr, uid, ids, c={}: ids, ['price_unit', 'discount', 'product_uom_qty', 'product_uos_qty'], 5),
+                'sale.order.line': (lambda self, cr, uid, ids, c={}: ids, [], 5000),
         }),
     }
