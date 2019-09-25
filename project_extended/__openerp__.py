9ead7143b56fcc813cff09b4a8c9b0327cb550d6
carlo <carlo.vettore@didotech.com>
2019-09-25 18:13:37 +0200

    [project_extened] fix sale order amount recalculation sequence

--- project_extended/__openerp__.py
+++ project_extended/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Project Extended',
-    'version': '3.16.25.30',
+    'version': '3.16.26.30',
     'category': 'Generic Modules/Projects & Services',
     'description': """Tasks list on a dedicated tab on the project form
 """,
