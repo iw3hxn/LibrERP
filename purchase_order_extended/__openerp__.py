ed28315d4a5004c21bbaa0425742e8c20749b8a6
carlo <carlo.vettore@didotech.com>
2019-12-09 16:18:28 +0100

    [purchase_order_extended] add also filter on purchase order

--- purchase_order_extended/__openerp__.py
+++ purchase_order_extended/__openerp__.py
@@ -24,7 +24,7 @@
 
 {
     "name": "Purchase Order Extended",
-    "version": "3.4.25.9",
+    "version": "3.4.25.10",
     "author": "Didotech SRL",
     "website": "http://www.didotech.com",
     "category": 'Purchase',
