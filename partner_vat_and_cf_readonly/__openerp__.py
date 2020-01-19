63679cf611e38c68c0d8fb9c4b96687b9f4f7907
andreinl <andrei.levin@didotech.com>
2020-01-19 21:18:15 +0100

    [partner_vat_and_cf_readonly] Initial commit

---
+++ partner_vat_and_cf_readonly/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Partner Vat and Fiscal Code readonly',
+    'version': "3.0.0.0",
+    'author': "Didotech SRL",
+    'website': "http://www.didotech.com",
+    'category': 'Partner',
+    'description': """
+     This module changes attribute of vat field and fiscal code field 'readonly' to false after an amount of minutes specified in system params with key 'vatcf_minutes_amount_readonly'
+    """,
+    'license': 'AGPL-3',
+    "depends": [
+        'base',
+        'account',
+        'partner_subaccount',
+        'l10n_it_account'
+    ],
+    "data": [
+        'views/vat_cf_view.xml',
+    ],
+    "demo": [],
+    "active": False,
+    "installable": True,
+    "application": True,
+}
+
