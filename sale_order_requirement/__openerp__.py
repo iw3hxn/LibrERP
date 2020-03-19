e8a541323024c5ec7bd4a7e669157705379de800
carlo <carlo.vettore@didotech.com>
2020-03-19 15:25:06 +0100

    [sale_order_requirement] block purchase order with minimun quantity

--- sale_order_requirement/__openerp__.py
+++ sale_order_requirement/__openerp__.py
@@ -1,6 +1,6 @@
 {
     "name": "Sale Order Requirement",
-    "version": "4.39.60.55",
+    "version": "4.39.61.56",
     "author": "Antonio Mignolli - Didotech SRL",
     "category": 'Sales Management',
     "description": """
--- sale_order_requirement/models/purchase_order.py
+++ sale_order_requirement/models/purchase_order.py
@@ -2,6 +2,7 @@
 # © 2017 Antonio Mignolli - Didotech srl (www.didotech.com)
 
 from openerp.osv import orm, fields
+from tools.translate import _
 
 
 class purchase_order(orm.Model):
@@ -10,6 +11,24 @@ class purchase_order(orm.Model):
     _columns = {
         'sale_order_ids': fields.many2many('sale.order', string='Sale Orders', readonly=True),
     }
+
+    def wkf_confirm_order(self, cr, uid, ids, context=None):
+        product_supplierinfo_obj = self.pool['product.supplierinfo']
+        product_uom = self.pool['product.uom']
+        for po in self.browse(cr, uid, ids, context=context):
+            partner_id = po.partner_id.id
+            for line in po.order_line:
+                qty = line.product_qty
+                product = line.product_id
+                supplierinfo_ids = product_supplierinfo_obj.search(cr, uid, [('name', '=', partner_id), ('product_id', '=', product.product_tmpl_id.id)])
+                if supplierinfo_ids:
+                    supplierinfo = product_supplierinfo_obj.browse(cr, uid, supplierinfo_ids[0], context=context)
+                    uom_id = line.product_uom.id
+                    min_qty = product_uom._compute_qty(cr, uid, supplierinfo.product_uom.id, supplierinfo.min_qty, to_uom_id=uom_id)
+                    if qty < min_qty:  # If the supplier quantity is greater than entered from user, set minimal.
+                        raise orm.except_orm(_('Error'), _('The selected supplier has a minimal quantity set to {qty} for product "{product}"').format(qty=min_qty, product=product.name))
+
+        return super(purchase_order, self).wkf_confirm_order(cr, uid, ids, context=context)
 
 
 class procurement_order(orm.Model):
