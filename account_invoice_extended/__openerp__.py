6dcc0fed2ec54150cb49cd0571081b636111bb37
carlo <carlo.vettore@didotech.com>
2020-09-30 16:27:43 +0200

    [account_invoice_extended] add active on form view

--- account_invoice_extended/__openerp__.py
+++ account_invoice_extended/__openerp__.py
@@ -24,7 +24,7 @@
 
 {
     "name": "Account Invoice extended",
-    "version": "3.7.20.22",
+    "version": "3.7.20.23",
     "author": "Didotech SRL",
     "website": "http://www.didotech.com",
     "category": "Generic Modules/Accounting",
