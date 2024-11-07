22f68a031603ef0c7358d27653f0eedb1d6cf074
carlo <carlo.vettore@didotech.com>
2024-11-07 11:32:50 +0100

    Refactor export logic for Fatturapa invoices
    Replaced methods _set_AltriDatiGestionali_line and setDatiGeneraliDocumento with a new _export_fatturapa_external method. Simplified the process by directly handling invoice line data and ensuring proper external code assignment. The new method reads invoice details from the fatturapa attachment, improving code clarity and maintainability.

--- account_account_external/models/wizard_export_fatturapa.py
+++ account_account_external/models/wizard_export_fatturapa.py
@@ -5,6 +5,7 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+from openerp.addons.l10n_it_ade.bindings import fatturapa_v_1_2_1 as fattpa
 from openerp.addons.l10n_it_ade.bindings.fatturapa_v_1_2_1 import (
     AltriDatiGestionaliType
 )
@@ -13,15 +14,23 @@ from openerp.osv import orm
 
 class WizardExportFatturapa(orm.TransientModel):
     _inherit = "wizard.export.fatturapa"
-    def _set_AltriDatiGestionali_line(self, cr, uid, DettaglioLinea, line, context):
-        res = super(WizardExportFatturapa, self)._set_AltriDatiGestionali_line(cr, uid, DettaglioLinea, line, context)
-        if line.invoice_id.type in ['in_invoice', 'in_refund']:
-            AltriDatiGestionali = AltriDatiGestionaliType(TipoDato='MASTRO', RiferimentoTesto=line.account_id.external_code or line.account_id.code)
-            DettaglioLinea.AltriDatiGestionali.append(AltriDatiGestionali)
-        return res
 
-    def setDatiGeneraliDocumento(self, cr, uid, invoice, body, context=None):
-        res = super(WizardExportFatturapa, self).setDatiGeneraliDocumento(cr, uid, invoice, body, context)
-        if invoice.type in ['in_invoice', 'in_refund']:
-            body.DatiGenerali.DatiGeneraliDocumento.Numero = invoice.supplier_invoice_number
-        return res
+    def _export_fatturapa_external(self, cr, uid, inv, context):
+        invoice_line_model = self.pool['account.invoice.line']
+        xml_string = inv.fatturapa_attachment_in_id.datas.decode('base64')
+        fatturapa = fattpa.CreateFromDocument(xml_string)
+        FatturaBody = fatturapa.FatturaElettronicaBody[0]
+        for line in FatturaBody.DatiBeniServizi.DettaglioLinee:
+            line_ids = invoice_line_model.search(cr, uid,
+                                                 [('name', '=', line.Descrizione), ('invoice_id', '=', inv.id)],
+                                                 limit=1)
+            if line_ids:
+                invoice_line = invoice_line_model.browse(cr, uid, line_ids[0], context=context)
+                external_code = invoice_line.account_id.external_code
+                if not external_code:
+                    external_code = 9999
+                AltriDatiGestionali = AltriDatiGestionaliType(TipoDato='MASTRO', RiferimentoTesto=external_code)
+                line.AltriDatiGestionali.append(AltriDatiGestionali)
+
+        number = inv.fatturapa_attachment_in_id.name
+        return number, fatturapa
